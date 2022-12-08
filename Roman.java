852a319639c551d2e4094fc7725a735d00a3206b
M-Samly <m.shamlymhmd@gmail.com>
2022-12-08 19:36:19 +0530

    Add new

--- Roman.java
+++ Roman.java
@@ -1,50 +1,72 @@
+//imports
 import java.util.Scanner;
 
 class Roman {
 
     public static void main(String[] args) {
-        //user input
-        Scanner sc = new Scanner(System.in);
-        String out = sc.nextLine();
-        //Method calling
-        int tot = calculate(out);
-        //print output
-        System.out.println(out+ " : "+tot);
+        //Scanner
+        Scanner s = new Scanner(System.in); 
+        // get user input
+        System.out.print("Enter Roman value: ");
+        String roman = s.nextLine(); 
+        // method call
+        int total = calInteger(roman); 
+        // print final value
+        System.out.println("Integer : " + total); 
+
     }
-    //methods
-    //create new method
-    static int calculate(String out){
-        //total initiate
-        int total = 0;
-        //for loop 
-        for (int i = 0; i < out.length(); i++) {
-            String num = String.valueOf(out.charAt(i));
-            //if else 
-            if (num.equalsIgnoreCase("I")) {
-                //calculate total
-                total = total + 1;
-            } else if (num.equalsIgnoreCase("V")) {
-                //calculate total
-                total = total + 5;
-            } else if (num.equalsIgnoreCase("X")) {
-                //calculate total
-                total = total + 10;
-            } else if (num.equalsIgnoreCase("L")) {
-                //calculate total
-                total = total + 50;
-            } else if (num.equalsIgnoreCase("C")) {
-                //calculate total
-                total = total + 100;
-            } else if (num.equalsIgnoreCase("D")) {
-                //calculate total
-                total = total + 500;
-            } else if (num.equalsIgnoreCase("M")) {
-                //calculate total
-                total = total + 1000;
-            }else{
-                System.out.println("Invalid Input");
-            }
+    
+    //checking roman values using method. method call rmChk
+    static int rmChk(char letter) { 
+        int val = 0;
+        String let = String.valueOf(letter);
+        // if conditions
+        if (let.equals("I")) {
+            val = 1;
+        } else if (let.equals("V")) {
+            val = 5;
+        } else if (let.equals("X")) {
+            val = 10;
+        } else if (let.equals("L")) {
+            val = 50;
+        } else if (let.equals("C")) {
+            val = 100;
+        } else if (let.equals("M")) {
+            val = 1000;
+        } else {
+            //invalid output printion
+            System.out.println("Invalid Letter: " + let);
         }
-        return total;
+        //return value
+        return val;
+    }
+
+    //calculate final integer values using method
+    static int calInteger(String roman) {
+        // set total val as 0
+        int total = 0; 
+        // for loop 
+        for (int i = 0; i < roman.length(); i++) { 
+            // get the value of frist romen char
+            int chk1 = rmChk(roman.charAt(i)); 
+            //// check if i + 1 less than to text length
+            if (i + 1 < roman.length()) { 
+                // get the value of second romen char
+                int chk2 = rmChk(roman.charAt(i + 1)); 
+                // compare two values
+                if (chk1 >= chk2) { 
+                     // if grater than second one add val1 to total
+                    total = total + chk1;
+                } else {
+                    // else subract val1 from total
+                    total = total -  chk1; 
+                }
+                // else i + 1 grater than text length add val1 to total
+            } else {
+                total = total + chk1; 
+            }
+
+        }
+        return total; // return final output
     }
 }
